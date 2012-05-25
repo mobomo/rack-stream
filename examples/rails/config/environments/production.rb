0f547b23993910eee2ce5dea309956e187d3eef3
Jerry Cheung <jch@whatcodecraves.com>
2012-05-24 23:32:57 -0700

    rails example serves static assets in production

--- examples/rails/config/environments/production.rb
+++ examples/rails/config/environments/production.rb
@@ -9,7 +9,7 @@ Chat::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -40,7 +40,7 @@ Chat::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :memory_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
